e1229d412b800755d39394f852a9b68e80dd4f9f
Bartha-coder <bartha.robert.dezso@student.ms.sapientia.ro>
2021-02-24 09:40:44 +0200

    elso hazi

---
+++ elsohazi/cmake-build-debug/CMakeFiles/elsohazi.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/elsohazi.dir/main.c.obj"
+  "elsohazi.exe"
+  "elsohazi.exe.manifest"
+  "elsohazi.pdb"
+  "libelsohazi.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/elsohazi.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
