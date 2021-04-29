ca1ccc9fb978a01af8e034c21a0135836e6d06eb
Bartha-coder <bartha.robert.dezso@student.ms.sapientia.ro>
2021-04-29 08:24:35 +0300

    semmi

---
+++ labor10/gyak10/cmake-build-debug/CMakeFiles/gyak10.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gyak10.dir/main.c.obj"
+  "gyak10.exe"
+  "gyak10.exe.manifest"
+  "gyak10.pdb"
+  "libgyak10.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/gyak10.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ labor10/gyak10/cmake-build-debug/CMakeFiles/gyak10.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for gyak10.
+# This may be replaced when dependencies are built.
