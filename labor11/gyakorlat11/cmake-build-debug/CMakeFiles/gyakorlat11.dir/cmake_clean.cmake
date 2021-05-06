bff1c1fbe54a32ca0e4ef45f66df064f67b38a6c
Bartha-coder <bartha.robert.dezso@student.ms.sapientia.ro>
2021-05-06 08:01:18 +0300

    labor 11 semmmi

---
+++ labor11/gyakorlat11/cmake-build-debug/CMakeFiles/gyakorlat11.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gyakorlat11.dir/main.c.obj"
+  "gyakorlat11.exe"
+  "gyakorlat11.exe.manifest"
+  "gyakorlat11.pdb"
+  "libgyakorlat11.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/gyakorlat11.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ labor11/gyakorlat11/cmake-build-debug/CMakeFiles/gyakorlat11.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for gyakorlat11.
+# This may be replaced when dependencies are built.
