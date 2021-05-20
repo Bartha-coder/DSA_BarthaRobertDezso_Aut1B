d3e7075060f5ddcb3ac05fd54472360c6dd43d8d
Bartha-coder <bartha.robert.dezso@student.ms.sapientia.ro>
2021-05-20 08:06:55 +0300

    semmi

---
+++ labor13/gyakorlat13/cmake-build-debug/CMakeFiles/gyakorlat13.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gyakorlat13.dir/main.c.obj"
+  "gyakorlat13.exe"
+  "gyakorlat13.exe.manifest"
+  "gyakorlat13.pdb"
+  "libgyakorlat13.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/gyakorlat13.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
