8879049a788b51b3211ab3db8fc72366e723d305
Bartha-coder <bartha.robert.dezso@student.ms.sapientia.ro>
2021-05-20 08:04:53 +0300

    nem

---
+++ onfejlesztes/extra12/cmake-build-debug/CMakeFiles/extra12.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/extra12.dir/main.c.obj"
+  "extra12.exe"
+  "extra12.exe.manifest"
+  "extra12.pdb"
+  "libextra12.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/extra12.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ onfejlesztes/extra12/cmake-build-debug/CMakeFiles/extra12.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for extra12.
+# This may be replaced when dependencies are built.
