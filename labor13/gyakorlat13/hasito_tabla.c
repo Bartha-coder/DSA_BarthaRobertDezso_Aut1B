c306d3611e2a6d9705a91b00ef9c7a590d863f20
Bartha-coder <bartha.robert.dezso@student.ms.sapientia.ro>
2021-05-20 09:13:38 +0300

    semmi2

---
+++ labor13/gyakorlat13/hasito_tabla.c
@@ -0,0 +1,5 @@
+//
+// Created by Bartha on 2021. 05. 20..
+//
+
+#include "hasito_tabla.h"
---
+++ labor13/gyakorlat13/hasito_tabla.h
@@ -0,0 +1,8 @@
+//
+// Created by Bartha on 2021. 05. 20..
+//
+
+#ifndef GYAKORLAT13_HASITO_TABLA_H
+#define GYAKORLAT13_HASITO_TABLA_H
+
+#endif //GYAKORLAT13_HASITO_TABLA_H
